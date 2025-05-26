a4d3cb1190b59bef2d4118481c98ae3a1b4ed2e0
Khoa Nguyen <123079882+khoanna@users.noreply.github.com>
2025-05-26 23:18:34 +0700

    Update Button.jsx

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Button = ({ styles }) => (
-  <Link to="/presale">
+  <Link to="/dex">
     <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
       Get Started
     </button>
