c2249e213d7b7ef942a188d55a6866fbdde3938a
Khoa Nguyen <123079882+khoanna@users.noreply.github.com>
2025-05-26 23:18:57 +0700

    Update GetStarted.jsx

--- frontend/src/components/GetStarted.jsx
+++ frontend/src/components/GetStarted.jsx
@@ -3,7 +3,7 @@ import { arrowUp } from "../assets";
 import { Link } from "react-router-dom";
 
 const GetStarted = () => (
-  <Link to="/presale">
+  <Link to="/dex">
     <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
       <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
         <div className={`${styles.flexStart} flex-row`}>
